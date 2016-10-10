d11b174f29b5ba9db96a86c9424972c508a441b7
Undrea <impromptuu@gmail.com>
2016-10-10 17:25:00 -0600

    Updated TransactionModel for new UserType declaration

--- DuoTab/Models/TransactionModel.swift
+++ DuoTab/Models/TransactionModel.swift
@@ -13,7 +13,8 @@ struct TransactionModel {
   var amount: Double
   var date: NSDate
   
-  var user: UserModel
+  /// Which user made the transaction
+  var user: UserType
   
   /// Keep track of how the amount should be divided; nil means there is no split. (e.g. "Me" pays for something, "SO" owes 100% of that back to "Me")
   var split: SplitModel?
