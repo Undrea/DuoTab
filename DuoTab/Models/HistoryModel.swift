eabb872251a60f726a6db3a4672f19721ed43ef1
Undrea <impromptuu@gmail.com>
2016-10-10 16:40:18 -0600

    Added HistoryModel
    Keeps a running history of expenses within the app

---
+++ DuoTab/Models/HistoryModel.swift
@@ -0,0 +1,14 @@
+//
+//  HistoryModel.swift
+//  DuoTab
+//
+//  Created by Isabella Hugel on 2016-10-10.
+//  Copyright © 2016 DuoTab. All rights reserved.
+//
+
+import Foundation
+
+/// Maintains the history of expenses and settlements between the couple.
+struct HistoryModel {
+  var transactions: [TransactionModel] = []
+}
