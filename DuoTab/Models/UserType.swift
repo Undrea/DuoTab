0acdfc69b07609ddf2146e3ca9187e160f930fc3
Undrea <impromptuu@gmail.com>
2016-10-10 16:39:45 -0600

    Added UserType
    Used to indicate the type of user (Me or SO)

---
+++ DuoTab/Models/UserType.swift
@@ -0,0 +1,18 @@
+//
+//  UserModel.swift
+//  DuoTab
+//
+//  Created by Isabella Hugel on 2016-10-10.
+//  Copyright © 2016 DuoTab. All rights reserved.
+//
+
+import Foundation
+
+/// Used to describe a user as either "Me" or "SO".
+///
+/// - Me: The individual using the app
+/// - SO: The individual's partner
+enum UserType {
+  case Me
+  case SO
+}
