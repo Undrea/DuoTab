13dc3ec2f297d41833f836ec61983dcac9415647
Undrea <impromptuu@gmail.com>
2016-10-10 17:25:33 -0600

    Added HomeViewModel and organized files in the explorer by name

--- DuoTab/ViewModels/HomeViewModel.swift
+++ DuoTab/ViewModels/HomeViewModel.swift
@@ -8,7 +8,31 @@
 
 import Foundation
 
-class HomeViewModel {
+struct HomeViewModel {
+  let balanceHeaderText: String
+  let balanceOwingAmount: String
+  let historyAbbreviated: [TransactionModel]
   
+  init(withBalance balance: BalanceModel, withHistory history: HistoryModel) {
+    if let userOwing = balance.userOwing {
+      switch userOwing {
+        case .Me:
+        balanceHeaderText = "Currently, I owe my partner:"
+        case .SO:
+        balanceHeaderText = "Currently, my partner owes me:"
+      }
+    } else { // No one currently owes anyone money
+      balanceHeaderText = "Balance:"
+    }
+    
+    balanceOwingAmount = balance.currencyValue ?? ""
+    
+    // Only show the last ten transactions at most
+    if history.transactions.count <= 10 {
+      historyAbbreviated = Array(history.transactions[0..<10])
+    } else {
+      historyAbbreviated = history.transactions
+    }
+  }
   
 }
