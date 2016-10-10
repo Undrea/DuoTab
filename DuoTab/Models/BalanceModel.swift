e4a72122191dd429ac383998879637a7e8d58bcc
Undrea <impromptuu@gmail.com>
2016-10-10 16:34:32 -0600

    Added BalanceModel
    Keeps track of the running tab between the couple

---
+++ DuoTab/Models/BalanceModel.swift
@@ -0,0 +1,15 @@
+//
+//  Balance.swift
+//  DuoTab
+//
+//  Created by Isabella Hugel on 2016-10-10.
+//  Copyright © 2016 DuoTab. All rights reserved.
+//
+
+import Foundation
+
+/// Maintains the running tab between the couple.
+struct BalanceModel {
+  /// If the balance is positive, then "Me" owes "SO", if the balance is negative, then "SO" owes "Me"
+  var amount: Double
+}
