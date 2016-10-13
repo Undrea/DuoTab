6ba16e280646552f0085cbd68dc9c0e8a06f167f
Undrea <impromptuu@gmail.com>
2016-10-12 20:16:16 -0600

    Customize programmatically the home "heart" expense buttons
    Positioning text and changing image colour for highlighted state

--- DuoTab/Views/HomeViewController.swift
+++ DuoTab/Views/HomeViewController.swift
@@ -14,8 +14,18 @@ class HomeViewController: UIViewController {
   @IBOutlet weak var settleButton: UIButton!
   @IBOutlet weak var contentContainer: UIView!
   
-  @IBOutlet weak var partnerPayButton: UIButton!
-  @IBOutlet weak var mePayButton: UIButton!
+  @IBOutlet weak var mePayButton: UIButton! {
+    didSet {
+      initialize(expenseButton: mePayButton, withImageNamed: "heartLeft")
+      mePayButton.titleEdgeInsets = UIEdgeInsets(top: 0, left: 5.0, bottom: 20.0, right: 0)
+    }
+  }
+  @IBOutlet weak var partnerPayButton: UIButton! {
+    didSet {
+      initialize(expenseButton: partnerPayButton, withImageNamed: "heartRight")
+      partnerPayButton.titleEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 20.0, right: 5.0)
+    }
+  }
   
   /// Initialize the view model with the saved balance (or 0) and the history of transactions
   let viewModel = HomeViewModel(withBalance: BalanceModel.loadBalance() ?? BalanceModel(amount: 0), withHistory: HistoryModel()) // TODO: Add real histroy
@@ -35,3 +45,16 @@ extension HomeViewController {
   
 }
 
+// MARK: - Helpers
+extension HomeViewController {
+  /// Use a single vector image and recolour it for the various "heart" expense button states.
+  ///
+  /// - parameter expenseButton: The expense-related button to set the state images for.
+  /// - parameter imageName:     Name of the image to apply to the button.
+  func initialize(expenseButton: UIButton, withImageNamed imageName: String) {
+    if let image = UIImage(named: imageName) {
+      expenseButton.setBackgroundImage(image.imageWithColor("#F44336".hexColor), for: UIControlState.normal)
+      expenseButton.setBackgroundImage(image.imageWithColor("#FF8A80".hexColor), for: UIControlState.highlighted)
+    }
+  }
+}
