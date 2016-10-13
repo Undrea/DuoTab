60c7c37aa09cdb3cf802ff342531cfbe1d09dfb6
Undrea <impromptuu@gmail.com>
2016-10-12 22:31:51 -0600

    Added unit tests for new BalanceModel getters.

--- DuoTabTests/BalanceModelTests.swift
+++ DuoTabTests/BalanceModelTests.swift
@@ -67,17 +67,20 @@ class BalanceModelTests: XCTestCase {
   
   /// Store Extension Tests
   func testSavingBalanceToNSUserDefaults() {
-    let balance = BalanceModel(amount: 10.0)
+    let balance = BalanceModel(amount: 12.5) // Passing in dollars
     balance.save(forKey: "TestBalanceKey")
+    
+    // Check the value stored there
+    XCTAssertEqual(UserDefaults.standard.object(forKey: "TestBalanceKey") as! Int, Int(12.5 * 100)) // Stored as cents
   }
   
   func testLoadingSavedBalance() {
     // Ensure key is in NSUserDefaults with value of 10.0
-    UserDefaults.standard.set(10.0, forKey: "TestBalanceKey")
+    UserDefaults.standard.set(123, forKey: "TestBalanceKey") // 123 is saved as cents, i.e. $1.23
     
     // Check if we can load that value
     if let balance = BalanceModel.loadBalance(forKey: "TestBalanceKey") {
-      XCTAssertEqual(balance.amount, 10.0)
+      XCTAssertEqual(balance.balanceInCents, 123)
     } else {
       XCTFail()
     }
@@ -89,5 +92,25 @@ class BalanceModelTests: XCTestCase {
 
     // Check if we can load that value
     XCTAssertNil(BalanceModel.loadBalance(forKey: "TestBalanceKey"))
+  }
+  
+  func testDollarsReturnsCorrectIntegerNumber() {
+    let balance = BalanceModel(amount: 123.90)
+    XCTAssertEqual(balance.dollars, "123")
+  }
+  
+  func testCentsReturnsCorrectRoundedIntegerNumber() {
+    let balance = BalanceModel(amount: 123.459)
+    XCTAssertEqual(balance.cents, "46") // Rounded to the nearest cent
+  }
+  
+  func testDollarsReturnsAsPositive() {
+    let balance = BalanceModel(amount: -123.90)
+    XCTAssertEqual(balance.dollars, "123")
+  }
+  
+  func testCentsReturnAsPositive() {
+    let balance = BalanceModel(amount: -123.459)
+    XCTAssertEqual(balance.cents, "46") // Rounded to the nearest cent
   }
 }
